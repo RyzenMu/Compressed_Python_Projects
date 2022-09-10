06deec7c69efb528e0085f53d5cdd62a135b2865
RyzenMu <muruga13in2022@gmail.com>
2022-09-10 20:12:16 +0530

    Contact_Book

---
+++ Beginner_Projects/Contact_Book.py
@@ -0,0 +1,9 @@
+# Create a contact book using json extension
+# The contact book helps to add, modify and delete contacts in the json file
+
+#import json file
+import json
+
+
+with open ('contacts.txt', 'w') as f:
+    f.write('A is for Amsterdam')
