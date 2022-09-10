9e389fad3fb89837652e7f91ef113988d0392f7b
RyzenMu <muruga13in2022@gmail.com>
2022-09-10 20:42:02 +0530

    few contacts added in the contact book

--- Beginner_Projects/Contact_Book.py
+++ Beginner_Projects/Contact_Book.py
@@ -1,9 +1,30 @@
 # Create a contact book using json extension
-# The contact book helps to add, modify and delete contacts in the json file
-
-#import json file
-import json
+# The contact book helps to add, modify and delete contacts using file handling
 
 
-with open ('contacts.txt', 'w') as f:
-    f.write('A is for Amsterdam')
+
+# Adding Contacts to the file
+with open ('contacts_name.xls', 'w') as f:
+    f.write('Amgstrong\n')
+    f.write('Bruce\n')
+    f.write('Ceaser\n')
+    f.write('Doug Lyod\n')
+    f.write('Ethan Hunt\n')
+
+
+with open ('contact_numbers.xls', 'w') as f:
+    f.write('987654321\n')
+    f.write('976543218\n')
+    f.write('965432187\n')
+    f.write('954321876\n')
+    f.write('943218765\n')
+
+
+
+with open ('contact_email.xls', 'w') as f:
+    f.write('amgstring@gmail.com\n')
+    f.write('bruce@gmail.com\n')
+    f.write('ceaser@gmail.com\n')
+    f.write('douglyod@gmail.com\n')
+    f.write('ethanhunt@gmail.com\n')
+
