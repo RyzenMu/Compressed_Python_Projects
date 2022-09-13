1429c49935fba454c896edb93e9f87a834e5eb59
RyzenMu <muruga13in2022@gmail.com>
2022-09-13 21:38:32 +0530

    Installing required packages completed

---
+++ Beginner_Projects/Email_Slicing.py
@@ -0,0 +1,15 @@
+# import requests
+import requests
+
+#import Beautiful Soup using pip install beautifulsoup4
+from bs4 import BeautifulSoup
+
+#import re
+import re
+
+
+# get request from linked in website
+
+# arrange email in an array/dictionary/set
+
+# validate the emails using regex
