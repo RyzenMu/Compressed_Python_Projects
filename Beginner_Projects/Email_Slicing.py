3ee776b3de403cb826a6a6bf5014656131d9baf8
RyzenMu <muruga13in2022@gmail.com>
2022-09-13 22:34:46 +0530

    Got contents of a page

--- Beginner_Projects/Email_Slicing.py
+++ Beginner_Projects/Email_Slicing.py
@@ -7,12 +7,38 @@ from bs4 import BeautifulSoup
 #import re
 import re
 
-page = requests.get('https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup')
+#import lxml
+import lxml
 
+
+
+
+# get request from a webpage
+page = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Python&txtLocation=')
+
+#print responce code to the request
 print(page)
 
 
-# get request from linked in website
+#Getting contents of a page
+page_content = page.content
+
+# print(page_content)
+
+
+# arrange the page in readable format using Beautifulsoup
+
+bs = BeautifulSoup(page_content, 'lxml')
+
+print(bs.prettify())
+
+
+
+
+
+
+
+
 
 # arrange email in an array/dictionary/set
 
