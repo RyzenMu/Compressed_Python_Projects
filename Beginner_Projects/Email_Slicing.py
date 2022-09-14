e5728cce536825bb164f284856bebd6eba0d4e52
RyzenMu <muruga13in2022@gmail.com>
2022-09-14 21:21:36 +0530

    got request 200 from timesjobs

--- Beginner_Projects/Email_Slicing.py
+++ Beginner_Projects/Email_Slicing.py
@@ -1,45 +1,11 @@
-# import requests
+import time
 import requests
-
-#import Beautiful Soup using pip install beautifulsoup4
 from bs4 import BeautifulSoup
 
-#import re
-import re
+html_page = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Python&txtLocation=').text
 
-#import lxml
-import lxml
+# print(html_page)
 
+soup = BeautifulSoup(html_page, 'lxml')
 
-
-
-# get request from a webpage
-page = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Python&txtLocation=')
-
-#print responce code to the request
-print(page)
-
-
-#Getting contents of a page
-page_content = page.content
-
-# print(page_content)
-
-
-# arrange the page in readable format using Beautifulsoup
-
-bs = BeautifulSoup(page_content, 'lxml')
-
-print(bs.prettify())
-
-
-
-
-
-
-
-
-
-# arrange email in an array/dictionary/set
-
-# validate the emails using regex
+print(soup)
