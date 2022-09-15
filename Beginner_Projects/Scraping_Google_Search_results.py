459b9de5786a9bd4afabb2dcad25fc66ea600010
RyzenMu <muruga13in2022@gmail.com>
2022-09-15 22:26:11 +0530

    searching google pages

---
+++ Beginner_Projects/Scraping_Google_Search_results.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+google_page = requests.get('https://www.google.com/search?channel=fs&client=ubuntu&q=how+to+find+email+in+facebook+profile').text
+
+soup = BeautifulSoup(google_page, 'lxml')
+
+results = soup.findAll('a', class_='fl')
+
+for result in results:
+    print(result.text)
