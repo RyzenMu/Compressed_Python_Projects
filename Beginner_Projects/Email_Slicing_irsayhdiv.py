770de4a11199ec88e51abfd1210afbb0fe21d177
RyzenMu <muruga13in2022@gmail.com>
2022-09-15 21:01:52 +0530

    new request for finding a name in google

---
+++ Beginner_Projects/Email_Slicing_irsayhdiv.py
@@ -0,0 +1,6 @@
+import requests
+
+
+vidhya = requests.get('https://www.google.com/search?q=linkedin+vidhyasri+ramalingam&client=ubuntu&channel=fs&ei=BUQjY7WoLOPw4-EPkMi6qA0&start=0&sa=N&ved=2ahUKEwj1vsHljZf6AhVj-DgGHRCkDtU4PBDy0wN6BAgBEDs&biw=1920&bih=968&dpr=1').text
+
+
