75a564c51339d851926b0c2c2706b5b7db6d4788
RyzenMu <muruga13in2022@gmail.com>
2022-09-16 21:21:47 +0530

    company website created

--- Beginner_Projects/Email_Slicing_FInd_Top_100_Companies.py
+++ Beginner_Projects/Email_Slicing_FInd_Top_100_Companies.py
@@ -3,7 +3,7 @@
 #  3. collect the html data from the web page
 #  4. use Beautiful soul and prettier() to find only the companies list 
 #  5. store the companies in a list
-#  6. use post method and enter into 100 webpages
+#  6. use request and enter into 100 webpages
 #  7. get webpage of 100 companies
 #  8. collect the html data of 100 companies
 #  9. find the email using regular expressions re module.
@@ -31,9 +31,35 @@ soup = BeautifulSoup(forbes_website, 'html.parser')
 all_divs = soup.find_all('div', class_="organizationName second table-cell name")
 
 
-for s_no, divs in enumerate(all_divs):
-    print(s_no, divs.text)
+# for s_no, divs in enumerate(all_divs):
+#     print(s_no, divs.text)
 
 
+#4.
+# prettier is not installed
+
+
+
+#5.
+company_list = []
+
+for divs in all_divs:
+    company_list.append(divs.text)
+# print(company_list)
+
+print(company_list[0])
+
+
+#list of company website
+company_website = []
+for company in company_list:
+    web_name = ''
+    for char in company:
+        if char == ' ':
+            continue
+        web_name += char.lower()
+    company_website.append('www.' + web_name + '.com')
+
+# print(company_website)
 
 
