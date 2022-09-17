d26b24a3fde6971953c6a7aec2ec49f0c020fbdc
RyzenMu <muruga13in2022@gmail.com>
2022-09-17 21:11:04 +0530

    logging into companies website

--- Beginner_Projects/Email_Slicing_FInd_Top_100_Companies.py
+++ Beginner_Projects/Email_Slicing_FInd_Top_100_Companies.py
@@ -47,19 +47,36 @@ for divs in all_divs:
     company_list.append(divs.text)
 # print(company_list)
 
-print(company_list[0])
+# print(company_list[0])
 
 
 #list of company website
-company_website = []
+company_websites = []
 for company in company_list:
     web_name = ''
     for char in company:
         if char == ' ':
             continue
         web_name += char.lower()
-    company_website.append('www.' + web_name + '.com')
+    company_websites.append('https://www.' + web_name + '.com')
 
-# print(company_website)
+# print(company_websites)
+
+
+#  6. use request and enter into 100 webpages
+# for company_website in company_websites:
+#     if company_website == 'www.saudiarabianoilcompany(saudiaramco).com':
+#         continue
+#     company_website_login = requests.get(company_website)
+#     print(company_website_login)
+
+
+for i in range(5, 100):
+    try:
+        company_website_login = requests.get(company_websites[i])
+        print(company_websites[i], company_website_login)
+    except:
+        print(company_websites[i] + 'error - unknown')
+
 
 
