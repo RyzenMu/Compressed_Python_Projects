e2f3fef3737a7ed86eff5c220f50edcc64fe90bb
RyzenMu <106828992+RyzenMu@users.noreply.github.com>
2022-09-24 11:00:40 +0530

    Update Desktop_Notifier.py

--- Beginner_Projects/Desktop_Notifier.py
+++ Beginner_Projects/Desktop_Notifier.py
@@ -12,4 +12,27 @@ path = os.getcwd()
 print(path)
 print(path)
 
+#Have you ever wondered how notifications work? This small python project idea will throw some light on this. The desktop notifier apps run on your system and send you a piece of information after a fixed interval of time. We suggest you use libraries such as notify2, requests, etc. to build such a program.
+
+import datetime
+from datetime import timedelta
+import os
+import notify2
+
+now = datetime.datetime.now().replace(microsecond=0)
+then = datetime.datetime(2022, 9, 24, 10, 59, 0)
+
+
+
+minutes = (diff.seconds - hours * 3600) / 60
+
+seconds = (diff.seconds - (hours * 3600 + minutes * 60))
+
+def sendmessage(title, message):
+    notify2.init("inage")
+    notice = notify2.Notification(title, message, "/Downloads/anne-nicole-AfOT-zVezTM-unsplash.jpg").show()
+    return notice
+
+sendmessage("Time Remaining", "%s days, %s hours, %s minutes and % seconds." % (diff.days,hours,minutes,seconds))
+
 
